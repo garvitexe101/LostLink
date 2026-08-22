9608f75996d1bce884789ec36ba9369ef05f55c1
Garvit <garvitnarula4@gmail.com>
2026-08-23 03:42:36 +0530

    Fix formatting of contact link event listener

--- js/how-it-works.js
+++ js/how-it-works.js
@@ -1,4 +1,4 @@
-/* How-it-works page navigation. */
+
 const contactLink = document.querySelector('a[href="#contact"]');
 if (contactLink) contactLink.addEventListener('click', () => document.querySelector('#contact')?.focus( {
   preventScroll:true
